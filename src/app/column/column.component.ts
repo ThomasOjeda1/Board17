f1da5f762d19db5b67554612816b22f39c2e006b
Thomas Ojeda <thomas.ojeda@globant.com>
2024-03-14 17:25:14 -0300

    minimal impl dragndrop material

--- src/app/column/column.component.ts
+++ src/app/column/column.component.ts
@@ -3,11 +3,11 @@ import { IssueComponent } from '../issue/issue.component';
 import { CommonModule } from '@angular/common';
 import { Issue, IssuesMockService } from '../issues-mock.service';
 import { Observable, tap } from 'rxjs';
-
+import { CdkDrag, CdkDragDrop, CdkDropList } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-column',
   standalone: true,
-  imports: [IssueComponent, CommonModule],
+  imports: [IssueComponent, CommonModule, CdkDrag, CdkDropList],
   templateUrl: './column.component.html',
   styleUrl: './column.component.scss',
 })
@@ -18,38 +18,25 @@ export class ColumnComponent {
 
   issues$!: Observable<Issue[]>;
   constructor(private issueService: IssuesMockService) {}
+  issues!: Issue[];
 
   ngOnInit() {
     this.issues$ = this.issueService.getIssues(this.columnName);
+    this.issueService.getIssues(this.columnName).subscribe((issues) => {
+      this.issues = issues;
+    });
   }
 
-  configureDragStartEvent($event: DragEvent) {
-    $event.dataTransfer?.setData('draggedElementId', ($event.target as any).id);
-  }
-
-  allowDrop(dragEndEvent: DragEvent) {
-    dragEndEvent.preventDefault();
-  }
-
-  finalizeDropOnZone($event: any) {
-    let draggedElementId = $event.dataTransfer?.getData('draggedElementId');
-    let droppedOnIssueId = this.issueService.getDroppedOnIssueIdAndClear();
-    if (droppedOnIssueId === null) {
-      this.issueService.moveIssueToColumn(draggedElementId, this.columnName);
-    } else {
-      this.issueService.moveIssueBeforeTargetInColumn(
-        draggedElementId,
-        droppedOnIssueId,
-        this.columnName
-      );
-
-      this.issueService.droppedOnIssueId = null;
-    }
-  }
-
-  registerTargetIssue($event: DragEvent) {
-    this.issueService.setDroppedOnIssueId(
-      ($event.currentTarget as HTMLElement).id
+  issueDropped($event: CdkDragDrop<string[]>) {
+    /*     console.log(
+      this.issues[$event.previousIndex].uniqueId,
+      this.issues[$event.currentIndex].uniqueId,
+      this.columnName
+    ); */
+    this.issueService.moveIssueBeforeTargetInColumn(
+      this.issues[$event.previousIndex].uniqueId,
+      this.issues[$event.currentIndex].uniqueId,
+      this.columnName
     );
   }
 }
